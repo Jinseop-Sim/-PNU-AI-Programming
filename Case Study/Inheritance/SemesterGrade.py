7505b841bebc0be377b998d6514450d3a829cafb
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2021-10-08 18:01:51 +0900

    Update and rename Case Study/SemesterGrade.py to Case Study/Inheritance/SemesterGrade.py

---
+++ Case Study/Inheritance/SemesterGrade.py
@@ -0,0 +1,27 @@
+import student
+
+def obtainListOfStudents():
+    listOfStudents = []
+    flag = 'Y'
+    while flag == 'Y':
+        name = input("Enter Student's name : ")
+        midterm = float(input("Enter grade on midterm : "))
+        final = float(input("Enter grade on final : "))
+        category = input("Enter category (LG or PF) : ")
+        if category.upper() == "LG":
+            st = student.LGStudent(name, midterm, final)
+        else:
+            st = student.PFStudent(name, midterm, final)
+        listOfStudents.append(st)
+        flag = input("Do you want to continue (Y/N)? ")
+        flag = flag.upper()
+    return listOfStudents
+
+def displayResults(listOfStudents):
+    print("\nNAME\tGRADE")
+    listOfStudents.sort(key=lambda x: x.getName())
+    for pupil in listOfStudents:
+        print(pupil)
+
+listOfStudents = obtainListOfStudents()
+displayResults(listOfStudents)
