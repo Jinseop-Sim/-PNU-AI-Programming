1c73fcf0ebbec87fa80ad6c53ea1742a38d4d02e
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2021-11-26 16:10:44 +0900

    Create optimizer.py

---
+++ Case Study/Hill Climbing Algorithm/With Class - Advanced(+Genetic)/optimizer.py
@@ -0,0 +1,386 @@
+#201724500 심진섭
+from numpy import random as rn
+from setup import Setup
+import random
+import math
+
+
+class Optimizer(Setup):
+    def __init__(self):
+        self._numRestart = 0
+        self._limitStuck = 100
+    def setVariables(self, parameters):
+        Setup.setVariables(self, parameters)
+        self._pType = parameters['pType']
+        self._numExp = parameters['numExp']
+        self._numRestart = parameters['numRestart']
+    def displaySetting(self):
+        pass
+    def run(self):
+        pass
+    def getNumExp(self):
+        return self._numExp
+    def displayNumExp(self):
+        print()
+        print('Number of experiments:', self._numExp)
+
+class HillClimbing(Optimizer):
+    def __init__(self):
+        Optimizer.__init__(self)
+
+    def randomRestart(self, p):
+        i = 1
+        self.run(p)  # 초기값을 설정하기 위해 self.run(p)을 한번 실행해준다.
+        bestSolution = p.getSolution()
+        bestMinimum = p.getMinimum()  # First result is current best
+        numEval = p.getNumEval()
+
+        while i < self._numRestart:
+            self.run(p)
+            newSolution = p.getSolution()
+            newMinimum = p.getMinimum()
+            numEval += p.getNumEval()
+
+            if newMinimum < bestMinimum:
+                bestSolution = newSolution
+                bestMinimum = newMinimum
+            i += 1
+
+        p.storeResult(bestSolution, bestMinimum)
+
+class MetaHeruistics(Optimizer):
+    def __init__(self):
+        Optimizer.__init__(self)
+        self.numSample = 5
+    def initTemp(self, p):
+        pass
+    def displaySetting(self):
+        print('Total number of evaluations until termination : ', self._limitEval)
+        print()
+    def getWhenBestFound(self):
+        pass
+
+class SteepestAscent(HillClimbing):
+    def __init__(self):
+        HillClimbing.__init__(self)
+
+    def displaySetting(self):
+        if self._pType == 1: # Numeric
+            print()
+            print("Search algorithm: Steepest-Ascent Hill Climbing")
+            print()
+            print('Number of random restarts: ', self._numRestart)
+            print()
+            print('Mutation step size:', self._delta)   # Setup Class에서 초기화한 self._delta변수 사용
+            print('Max evaluations with no improvement: ',self._limitStuck, end='')
+            print(' iterations')
+            print()
+
+        elif self._pType == 2: # TSP
+            print()
+            print("Search algorithm: Steepest-Ascent Hill Climbing")
+            print()
+            print('Number of random restarts: ', self._numRestart)
+            print()
+            print('Max evaluations with no improvement: ', self._limitStuck, end='')
+            print(' iterations')
+            print()
+
+    def setVariables(self, aType, pType):
+        self._aType = aType
+        self._pType = pType
+
+    def run(self, p):
+        current = p.randomInit()  # A current candidate solution
+        valueC = p.evaluate(current)
+        while True:
+            neighbors = p.mutants(current)
+            successor, valueS = self.bestOf(neighbors, p)
+            if valueS >= valueC:
+                break
+            else:
+                current = successor
+                valueC = valueS
+        p.storeResult(current, valueC)
+
+    def bestOf(self, neighbors, p):
+        best = neighbors[0]
+        bestValue = p.evaluate(best)
+        for i in range(1, len(neighbors)):
+            newValue = p.evaluate(neighbors[i])
+            if newValue < bestValue:
+                best = neighbors[i]
+                bestValue = newValue
+        return best, bestValue
+
+class FirstChoice(HillClimbing):
+    def __init__(self):
+        HillClimbing.__init__(self)
+
+    def displaySetting(self):
+        if self._pType == 1:  # Numeric
+            print()
+            print("Search algorithm: First-Choice Hill Climbing")
+            print()
+            print('Number of random restarts: ', self._numRestart)
+            print()
+            print('Mutation step size:', self._delta)  # Setup Class에서 초기화한 self._delta변수 사용
+            print('Max evaluations with no improvement: ', self._limitStuck, end='')
+            print(' iterations')
+            print()
+
+        elif self._pType == 2:  # TSP
+            print()
+            print("Search algorithm: First-Choice Hill Climbing")
+            print()
+            print('Number of random restarts: ', self._numRestart)
+            print()
+            print('Max evaluations with no improvement: ', self._limitStuck, end='')
+            print(' iterations')
+            print()
+
+    def run(self, p):
+        current = p.randomInit()
+        valueC = p.evaluate(current)
+        i = 0
+        f = open('FC.txt', 'w')
+        while i < self._limitStuck:
+            f.write(str(valueC) + '\n')
+            successor = p.randomMutant(current)
+            valueS = p.evaluate(successor)
+            if valueS < valueC:
+                current = successor
+                valueC = valueS
+                i = 0
+            else:
+                i += 1
+        p.storeResult(current, valueC)
+        f.close()
+
+class GradientDescent(HillClimbing):
+    def __init__(self):
+        HillClimbing.__init__(self)
+
+    def displaySetting(self):
+        print()
+        print("Search Algorithm: Gradient Descent")
+        print()
+        print("Update rate:", self._alpha)
+        print("Increment for calculating derivatives:", self._dx)
+
+    def run(self, p):
+        currentP = p.randomInit()  # Current point
+        valueC = p.evaluate(currentP)
+        while True:
+            nextP = p.takeStep(currentP, valueC)
+            valueN = p.evaluate(nextP)
+            if valueN >= valueC:
+                break
+            else:
+                currentP = nextP
+                valueC = valueN
+        p.storeResult(currentP, valueC)
+
+class Stochastic(HillClimbing):
+    def __init__(self):
+        HillClimbing.__init__(self)
+
+    def displaySetting(self):
+        if self._pType == 1:  # Numeric
+            print()
+            print("Search algorithm: Stochastic Algorithm")
+            print()
+            print('Number of random restarts: ', self._numRestart)
+            print()
+            print('Mutation step size:', self._delta)  # Setup Class에서 초기화한 self._delta변수 사용
+            print('Max evaluations with no improvement: ', self._limitStuck, end='')
+            print(' iterations')
+            print()
+
+        elif self._pType == 2:  # TSP
+            print()
+            print("Search algorithm: Stochastic Algorithm")
+            print()
+            print('Number of random restarts: ', self._numRestart)
+            print()
+            print('Max evaluations with no improvement: ', self._limitStuck, end='')
+            print(' iterations')
+            print()
+
+    def run(self, p):
+        current = p.randomInit()
+        valueC = p.evaluate(current)
+        while True:
+            neighbors = p.mutants(current)
+            successor, valueS = self.stochasticBest(neighbors, p)
+            if valueS >= valueC:
+                break
+            else:
+                current = successor
+                valueC = valueS
+        p.storeResult(current, valueC)
+
+    def stochasticBest(self, neighbors, p):
+        # Smaller valuse are better in the following list
+        valuesForMin = [p.evaluate(indiv) for indiv in neighbors]
+        largeValue = max(valuesForMin) + 1
+        valuesForMax = [largeValue - val for val in valuesForMin]
+        # Now, larger values are better
+        total = sum(valuesForMax)
+        randValue = random.uniform(0, total)
+        s = valuesForMax[0]
+        for i in range(len(valuesForMax)):
+            if randValue <= s: # The one with index i is chosen
+                break
+            else:
+                s += valuesForMax[i+1]
+        return neighbors[i], valuesForMin[i]
+
+class SimulatedAnnealing(MetaHeruistics):
+    def __init__(self):
+        MetaHeruistics.__init__(self)
+        self._numSample = 100
+        self._whenBest = 0
+
+    def displaySetting(self):
+        print()
+        print("Search algorithm: Simulated Annealing")
+        print()
+        print('Number of random restarts: ', self._numRestart)
+        print()
+        print('Limited Total number of evaluations until termination : ', self._limitEval)
+        print()
+        print('Number of times executed to get the best value : ', self._whenBest)
+        print()
+        print("Increment for calculating derivatives: ", self._dx)  # Setup Class에서 초기화한 self._dx변수 사용
+        print('Max evaluations with no improvement: {}'.format(self._limitStuck), end='')
+        print(' iterations')
+        print()
+
+    def run(self, p):
+        current = p.randomInit()
+        valueC = p.evaluate(current)
+        temp = self.initTemp(p)
+        best, valueBest = current, valueC
+        whenBest = i = 1
+        f = open('SA.txt', 'w')
+
+        while True:
+            f.write(str(valueC) + '\n')
+            neighbor = p.randomMutant(current)
+            valueN = p.evaluate(neighbor)
+            i += 1
+            temp = self.tSchedule(temp)
+
+            if temp == 0 or i == self._limitEval:
+                break
+            dE = valueN - valueC
+            if dE < 0:
+                current = neighbor
+                valueC = valueN
+            elif random.uniform(0, 1) < math.exp(-dE / temp):
+                current = neighbor
+                valueC = valueN
+            if valueC < valueBest:
+                (best, valueBest) = (current, valueC)
+                whenBest = i
+
+        self._whenBest = whenBest
+        p.storeResult(best, valueBest)
+        f.close()
+
+    def initTemp(self, p): # To set initial acceptance probability to 0.5
+        diffs = []
+        for i in range(self._numSample):
+            c0 = p.randomInit()     # A random point
+            v0 = p.evaluate(c0)     # Its value
+            c1 = p.randomMutant(c0) # A mutant
+            v1 = p.evaluate(c1)     # Its value
+            diffs.append(abs(v1 - v0))
+        dE = sum(diffs) / self._numSample  # Average value difference
+        t = dE / math.log(2)        # exp(–dE/t) = 0.5
+        return t
+
+    def getWhenBestFound(self):
+        return self._whenBest
+
+    def tSchedule(self, t):
+        return t * (1 - (1 / 10**4))
+
+class Genetic(MetaHeruistics):
+    def __init__(self):
+        MetaHeruistics.__init__(self)
+        self._popSize = 0
+        self._uXp = 0
+        self._mrF = 0
+        self._XR = 0
+        self._mR = 0
+        self._pC = 0
+        self._pM = 0
+
+    def setVariables(self, parameters):
+        MetaHeruistics.setVariables(self, parameters)
+        self._limitEval = parameters['limitEval']
+        self._popSize = parameters['popSize']
+        self._resolution = parameters['resolution']
+        self._uXp = parameters['uXp']
+        self._mrF = parameters['mrF']
+        self._XR = parameters['XR']
+        self._mR = parameters['mR']
+
+        if self._pType == 1:
+            self._pC = self._uXp
+            self._pM = self._mrF
+        if self._pType == 2:
+            self._pC = self._XR
+            self._pM = self._mR
+
+    def displaySetting(self):
+        print()
+        print("Search Algorithm: Genetic Algorithm")
+        print()
+        MetaHeruistics.displaySetting(self);
+        print()
+        print("Population size:", self._popSize)
+        if self._pType == 1:   # Numerical optimization
+            print("Number of bits for binary encoding:", self._resolution)
+            print("Swap probability for uniform crossover:", self._uXp)
+            print("Multiplication factor to 1/L for bit-flip mutation:",
+                  self._mrF)
+        elif self._pType == 2: # TSP
+            print("Crossover rate:", self._XR)
+            print("Mutation rate:", self._mR)
+
+    def run(self, p):
+        population = p.initializePop(self._popSize)
+        ch = 0
+        while ch < self._limitEval:
+            newPop = []
+            fitSum = 0
+            for i in range(0, len(population)):
+                p.evalInd(population[i])
+                fitSum = fitSum + population[i][0]
+            population.sort(reverse = True)
+
+            for i in range(0, len(population)):
+                x, y = self.randomSelection(population, fitSum)
+                child_x, child_y = p.crossover(x, y, self._uXp)
+                child = p.mutation(child_x, self._mrF)
+                newPop.append(child)
+            population = newPop
+            ch += 1
+
+        value = p.indToSol(population[0])
+        bestValue = p.evaluate(value)
+        p.storeResult(value, bestValue)
+
+    def randomSelection(self, population, fitSum):
+        prob = []
+        probIdx = []
+
+        for i in range(len(population)):
+            probIdx.append(i)
+            prob.append(population[i][0]/fitSum)
+
+        x, y = rn.choice(probIdx, 2, p=prob, replace=False)
+        return population[x], population[y]
